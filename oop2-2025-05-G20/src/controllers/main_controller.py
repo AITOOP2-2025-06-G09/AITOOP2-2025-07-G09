020456f7cf998526e7dc4d11c465a2c7646b1a02
natsumeg77 <r2383972@gmail.com>
2025-11-20 14:41:18 +0900

    エラー修正とその他のファイル追加

--- oop2-2025-05-G20/src/controllers/main_controller.py
+++ oop2-2025-05-G20/src/controllers/main_controller.py
@@ -1,1 +1,30 @@
 ## ロジック
+from src.models.image_processor import ImageProcessor
+import cv2
+
+class MainController:
+    def __init__(self, ui):
+        self.ui = ui
+        self.processor = ImageProcessor()
+        self.capture_img = None
+
+        ui.btn_capture.clicked.connect(self.capture)
+        ui.btn_process.clicked.connect(self.process)
+
+    def capture(self):
+        from my_module.K24117.lecture05_camera_image_capture import MyVideoCapture
+        cap = MyVideoCapture()
+        cap.run()
+        self.capture_img = cap.get_img()
+        self.ui.show_image(self.capture_img)
+
+    def process(self):
+        if self.capture_img is None:
+            return
+        
+        result = self.processor.replace_white(
+            "images/google.png",
+            self.capture_img
+        )
+        self.ui.show_image(result)
+        cv2.imwrite("my_module/K24117/output_images/lecture05_01_output.png", result)
--- oop2-2025-05-G20/src/ui/main_window.py
+++ oop2-2025-05-G20/src/ui/main_window.py
@@ -1,1 +1,31 @@
 ## UI
+from PySide6.QtWidgets import QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout
+from PySide6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("白色置換アプリ")
+
+        self.label = QLabel("画像がここに表示されます")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.btn_capture = QPushButton("カメラキャプチャ")
+        self.btn_process = QPushButton("白色置換開始")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.btn_capture)
+        layout.addWidget(self.btn_process)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+    def show_image(self, cv_img):
+        from PySide6.QtGui import QImage, QPixmap
+        
+        h, w, ch = cv_img.shape
+        bytes_per_line = ch * w
+        qimg = QImage(cv_img.data, w, h, bytes_per_line, QImage.Format_BGR888)
+        self.label.setPixmap(QPixmap.fromImage(qimg))
