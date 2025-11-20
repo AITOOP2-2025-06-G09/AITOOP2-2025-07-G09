8d250ace9815631e9872ebb4d6a2e2752ffc2624
shion4949 <wshitou456@gmail.com>
2025-11-20 14:31:03 +0900

    メイン変更

--- oop2-2025-05-G20/main.py
+++ oop2-2025-05-G20/main.py
@@ -1,4 +1,19 @@
-from src.lecture05_01 import lecture05_01
+## from src.lecture05_01 import lecture05_01
+
+## if __name__ == "__main__":
+##    lecture05_01()
+
+from PySide6.QtWidgets import QApplication
+from src.ui.main_window import MainWindow
+from src.controllers.main_controller import MainController
+
+def main():
+    app = QApplication([])
+    ui = MainWindow()
+    controller = MainController(ui)
+
+    ui.show()
+    app.exec()
 
 if __name__ == "__main__":
-    lecture05_01()
+    main()
